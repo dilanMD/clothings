e70764cc7c052385ee676b2141ec6b247bac26b5
dilanMD <deluxan.sgic@gmail.com>
2020-04-09 14:43:38 +0530

    React Router Dom implemented with MenuItem

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -10,27 +10,32 @@ class Directory extends Component {
                 {
                     id: 1,
                     title: 'hats',
+                    linkUrl: 'hats',
                     imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLzS5nqk6VlF-W1eJhJkhV-pXnI_NTrdM1nd1eEaYccw2RQiuE&usqp=CAU'
                 },
                 {
                     id: 2,
                     title: 'jackets',
+                    linkUrl: '',
                     imageUrl: 'https://previews.123rf.com/images/klyots/klyots1808/klyots180800050/106492717-shopping-sale-mall-concept-denim-jeans-clothes-on-store-shelves-faux-leather-jackets.jpg'
                 },
                 {
                     id: 3,
                     title: 'sneakers',
+                    linkUrl: '',
                     imageUrl: 'https://www.n1citymall.co.za/images/n1-city-mall-converse-nike-adidas-puma-vans-shoes.jpg'
                 },
                 {
                     id: 4,
                     title: 'women',
+                    linkUrl: '',
                     size: 'large',
                     imageUrl: 'https://files.stocky.ai/uploads/2019/02/image-Women-shopping-in-mall-stocky-ai-26206050.jpg'
                 },
                 {
                     id: 5,
                     title: 'men',
+                    linkUrl: '',
                     size: 'large',
                     imageUrl: 'https://st.depositphotos.com/1016231/4630/i/950/depositphotos_46308145-stock-photo-three-young-male-fashion-metraseksuals.jpg'
                 }
@@ -40,8 +45,8 @@ class Directory extends Component {
 
     render() {
         return (
-            this.state.sections.map(({ id, title, imageUrl, size }) => (
-                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+            this.state.sections.map(({ id, ...sectionProps }) => (
+                <MenuItem key={id} {...sectionProps} />
             ))
         )
     }
